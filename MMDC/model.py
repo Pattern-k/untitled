bf4e37dee0447a21496c2bfcd129570f840aab19
Pattern-k <wk15020291236@126.com>
2021-05-27 19:32:47 +0800

    Initial commit

---
+++ MMDC/model.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+
+from tensorflow.keras.layers import Conv2D,  BatchNormalization, Activation
+from tensorflow.keras.layers import Concatenate, AveragePooling2D,Conv2DTranspose
+
+
+def Inception(inputs):
+
+    branch1 = Conv2D(32,1,padding="same")(inputs)
+    branch1 = BatchNormalization()(branch1)
+    branch1 = Activation("relu")(branch1)
+
+
+    branch2 = Conv2D(32,1,padding="same")(inputs)
+    branch2 = BatchNormalization()(branch2)
+    branch2 = Activation("relu")(branch2)
+
+    branch2 = Conv2D(32,kernel_size=(1,3),padding="same")(branch2)
+    branch2 = BatchNormalization()(branch2)
+    branch2 = Activation("relu")(branch2)
+
+    branch2 = Conv2D(32,kernel_size=(3,1),padding="same")(branch2)
+    branch2 = BatchNormalization()(branch2)
+    branch2 = Activation("relu")(branch2)
+
+
+
+#
+    branch3 = Conv2D(32, 1, padding="same")(inputs)
+    branch3 = BatchNormalization()(branch3)
+    branch3 = Activation("relu")(branch3)
+
+    branch3 = Conv2D(32, kernel_size=(1, 3), padding="same")(branch3)
+    branch3 = BatchNormalization()(branch3)
+    branch3 = Activation("relu")(branch3)
+
+    branch3 = Conv2D(32, kernel_size=(3, 1), padding="same")(branch3)
+    branch3 = BatchNormalization()(branch3)
+    branch3 = Activation("relu")(branch3)
+
+
+    x = Concatenate(axis=3)([branch1,branch2,branch3])
+    return x
+
+def Downsampling_block(Down,filters):
+
+    down1 = AveragePooling2D(2,strides=2)(Down)
+
+    down2 = Conv2D(filters,1,padding="same")(Down)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation("relu")(down2)
+
+    down2 = Conv2D(filters,3,strides=2,padding="same")(down2)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation("relu")(down2)
+
+    down3 = Conv2D(filters, 1, padding="same")(Down)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation("relu")(down3)
+
+    down3 = Conv2D(filters, 3,strides=2, padding="same")(down3)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation("relu")(down3)
+
+
+
+    down =  Concatenate(axis=3)([down1,down2,down3])
+    return down
+
+
+
+
+def dense_block(dense):
+    x1 = Inception(dense)
+    concat1 = Concatenate(axis=3)([dense,x1])
+    x2 = Inception(concat1)
+    concat2 = Concatenate(axis=3)([dense,x1,x2])
+    x3 = Inception(concat2)
+    concat3 = Concatenate(axis=3)([dense,x1,x2,x3])
+    return concat3
+
+def create_model():
+    inputs = tf.keras.layers.Input(shape=(256,256,3))
+
+    conv1 = Inception(inputs)
+
+    dense_block1 = dense_block(conv1)
+    down1 = Downsampling_block(dense_block1,filters=256)
+
+    dense_block2 = dense_block(down1)
+    down2 = Downsampling_block(dense_block2,filters=256)
+
+    dense_block3 = dense_block(down2)
+    down3 = Downsampling_block(dense_block3,filters=256)
+
+    dense_block4 = dense_block(down3)
+    down4 = Downsampling_block(dense_block4,filters=256)
+
+    dense_block5 = dense_block(down4)
+
+    up1 = Conv2DTranspose(512,3,strides=2,activation="relu",padding="same")(dense_block5)
+    up_concat1 = Concatenate(axis=3)([up1,dense_block4])
+    up_b1 = Inception(up_concat1)
+    up_dense1 = dense_block(up_b1)
+
+
+    up2 = Conv2DTranspose(512,3,strides=2,activation="relu",padding="same")(up_dense1)
+    up_concat2 = Concatenate(axis=3)([up2,dense_block3])
+    up_b2 = Inception(up_concat2)
+    up_dense2 = dense_block(up_b2)
+
+    up3 = Conv2DTranspose(512,3,strides=2,activation="relu",padding="same")(up_dense2)
+    up_concat3 = Concatenate(axis=3)([up3,dense_block2])
+    up_b3 = Inception(up_concat3)
+    up_dense3 = dense_block(up_b3)
+
+
+    up4 = Conv2DTranspose(512,3,strides=2,activation="relu",padding="same")(up_dense3)
+    up_concat4 = Concatenate(axis=3)([up4,dense_block1])
+    up_b4 = Inception(up_concat4)
+    up_dense4 = dense_block(up_b4)
+
+    prediction = tf.keras.layers.Convolution2D(2, 1, padding="same", activation="softmax")(up_dense4)  # 256*256*34
+    return tf.keras.Model(inputs=inputs, outputs=prediction)
+
+model = create_model()
+
+
+# print(model.summary())
+# tf.keras.utils.plot_model(model)  #绘制模型流程图
+
+
+
---
+++ MMDC/train.py
@@ -0,0 +1,96 @@
+
+import tensorflow as tf
+import numpy as np
+import glob
+from Unet import model
+
+
+
+def data_process():
+
+    image = glob.glob("F:/data_segmentation/image/*")
+    # 排序
+    image.sort(key=lambda x: x.split("/")[-1])
+    label = glob.glob("F:/data_segmentation/label/*")
+
+    label.sort(key=lambda x: x.split("/")[-1])
+
+
+    np.random.seed(2020)
+    index = np.random.permutation(len(image))
+
+    image = np.array(image)[index]
+    label = np.array(label)[index]
+
+
+    dataset = tf.data.Dataset.from_tensor_slices((image,label))
+
+
+    test_count= int(len(image)*0.2)
+    train_count= len(image)-test_count
+
+    train_ds = dataset.skip(test_count)
+    test_ds = dataset.take(test_count)
+
+
+    train_ds = train_ds.map(load_img,num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    test_ds = test_ds.map(load_img)
+
+    batch_size = 8
+
+    buffer_size = 200
+    STEPS_PER_EPOCH = train_count//batch_size
+    VALIDATION_STEPS = test_count//batch_size
+
+
+    train_ds = train_ds.cache().shuffle(buffer_size).batch(batch_size).repeat()
+    train_ds = train_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    test_ds = test_ds.batch(batch_size)
+    return train_ds,test_ds,STEPS_PER_EPOCH,VALIDATION_STEPS
+
+def read_img(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_png(img,channels=3)
+    return img
+def read_label(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_png(img,channels=1)
+    return img
+
+def normalize(img,mask):
+    img = tf.cast(img,tf.float32)/127.5-1
+    mask = tf.cast(mask,tf.float32)
+    return img,mask
+
+def load_img(img_path,label_path):
+    img = read_img(img_path)
+    img = tf.image.resize(img,(256,256))
+
+    label = read_label(label_path)
+    label = tf.image.resize(label,(256,256))
+
+    img,label = normalize(img,label)
+    return img,label
+
+model = model.create_model()
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001,beta_1=0.999),
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+
+train_dataset,test_dataset,STEPS_PER_EPOCH,VALIDATION_STEPS =data_process()
+
+epoch =120
+
+
+
+history = model.fit(train_dataset,
+                    steps_per_epoch=STEPS_PER_EPOCH,
+                    epochs= epoch,
+                    validation_data=test_dataset,
+                    validation_steps=VALIDATION_STEPS)
+
+model.save_weights("F:/data_segmentation/save_weight/MMDC-Unet.h5")
+
+
+
